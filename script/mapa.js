17cc9db31b4d8aa68097f37efc678356a6247935
Andres Ardila <andres.ardila.agudelo@gmail.com>
2022-02-03 01:39:48 -0500

    trabajo final

---
+++ script/mapa.js
@@ -0,0 +1,43 @@
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
+function Punto1() {
+	document.getElementById("Info").innerHTML = '<iframe class="cuatro" src="https://www.youtube.com/embed/iRmRGP9hzy8" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>Las <a>Podracing</a> eran un deporte popular en las regiones menos desarrolladas de la galaxia y fue uno de los deportes más peligrosos jamás inventados. conoce mas acerca de <a href="actividades.html#podracing">podracing</a>.';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el segundo punto.
+function Punto2() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" src="imagenes/MosEspa.png" alt="Mos Espa"><a>mos espa</a> era un asentamiento de puerto espacial en <a>tatooine</a>. el asentamiento incluía una serie de espacios comerciales y de trabajo, así como establecimientos de entretenimiento como el <a>mos espa grand arena</a>.';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el tercer punto.
+function Punto3() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" width="50%" src="imagenes/MosEsley.png" alt="Mos Esley"><a>Mos Eisley</a> era un puerto espacial en <a>tatooine</a>. estaba ubicado en la gran meseta de mesra, al norte de anchorhead y al sureste de jundland wastes. el puerto espacial, aunque carecía de una instalación de aterrizaje principal, contenía 362 hangares en los que podían atracar los recien llegados.<br><br>la ciudad tenía calles anchas y arenosas y muchos edificios de varios tamaños. <a>Mos Eisley</a> fue el hogar de varias especies de extraterrestres de toda la galaxia.';
+	document.getElementById("Info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto4() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" width="30%" src="imagenes/jpalace.jpg" alt="Jabba">el <a>palacio de jabba</a>, estaba ubicado en la periferia del mar de las dunas del norte en mos espa en el planeta tatooine. originalmente erigido como un monasterio, se convirtió en una base de operaciones para <a href="habitantes.html#hutt>jabba</a> y <a>boba fett</a>.';
+	document.getElementById("Info").scrollIntoView();
+}
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto5() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" width="30%" src="imagenes/farm.png" alt="Farm">una <a>granja de humedad</a> era un área de tierra dedicada a la producción de agua a través de la extracción de humedad del aire seco. conoce de <a href="actividades.html#cultivo">granjas de humedad</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto6() {
+	document.getElementById("Info").innerHTML = '"una vez descrito como el lugar más alejado del brillante centro del universo, el remoto planeta desértico <a>tatooine</a> se hornea bajo sus soles gemelos. es el hogar de dos especies nativas, <a>jawas</a> y <a>tuskens</a>, así como de una amplia variedad de recién llegados. <a>tatooine</a> es famoso por ser el mundo natal de dos grandes jedi: <a>anakin</a> y <a>luke skywalker</a>."';
+	document.getElementById("Info").scrollIntoView();
+}
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto7() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" width="35%" src="imagenes/tusken.jpg" alt="Tusken">Los <a>Tusken Raiders</a>, nativos del planeta, eran una sociedad tribal nómada con estrictos códigos sociales y morales. <br><br>conoce más de los <a href="habitantes.html#tusken">tusken</a>';
+	document.getElementById("Info").scrollIntoView();
+}
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto8() {
+	document.getElementById("Info").innerHTML = '<img class="cuatro" width="20%" src="imagenes/Jawa.png" alt="jawas">Los Jawas también era una especie nativa de Tatooine y estaban profundamente arraigados a un estilo de vida nómada. conoce de <a href="habitantes.html#jawas"> los jawa</a>';
+	document.getElementById("Info").scrollIntoView();
+}
